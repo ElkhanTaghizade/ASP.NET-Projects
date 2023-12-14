67f33bb3cefb4d817b9e5503a3860449c499eb9b
ElkhanTaghizade <140736203+ElkhanTaghizade@users.noreply.github.com>
2023-12-14 15:31:12 +0400

    Add files via upload

---
+++ UOMO.Commerce/App/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication5.Models;
+
+namespace WebApplication5.App
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)  { }
+        public DbSet<Data> Data { get; set; }
+        public DbSet<Categories> Categories { get; set; }  
+        public DbSet<Products> Products { get; set; }
+
+    }
+}
---
+++ UOMO.Commerce/Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using WebApplication5.App;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public readonly AppDbContext _appdbContext;
+        public HomeController(ILogger<HomeController> logger, AppDbContext appDbContext)
+        {
+            _logger = logger;
+            _appdbContext = appDbContext;
+        }
+
+        public async Task <IActionResult> Index1()
+        {
+            List<Products> products = await _appdbContext.Products.Include(x=>x.Categories).ToListAsync();
+            return View(products);
+        }
+        public IActionResult login_register()
+        {
+            List<Data> list = _appdbContext.Data.ToList();
+            return View(list);
+        }
+        public IActionResult about()
+        {
+            return View();
+        }
+        public IActionResult account_dashboard()
+        {
+            return View();
+        }
+        public IActionResult account_edit()
+        {
+            return View();
+        }
+        public IActionResult account_edit_adress()
+        {
+            return View();
+        }
+        public IActionResult account_orders()
+        {
+            return View();
+        }
+        public IActionResult account_wishlist()
+        {
+            return View();
+        }
+        public IActionResult blog_list2()
+        {
+            return View();
+        }
+        public IActionResult blog_single()
+        {
+            return View();
+        }
+        public IActionResult coming_soon()
+        {
+            return View();
+        }
+        public IActionResult contact()
+        {
+            return View();
+        }
+        public IActionResult faq()
+        {
+            return View();
+        }
+        public IActionResult product2_variable()
+        {
+            return View();
+        }
+        public IActionResult product6_outofstock()
+        {
+            return View();
+        }
+        public IActionResult reset_password()
+        {
+            return View();
+        }
+        public IActionResult shop_cart()
+        {
+            return View();
+        }
+        public IActionResult shop_checkout()
+        {
+            return View();
+        }
+        public IActionResult shop_order_complete()
+        {
+            return View();
+        }
+        public IActionResult shop12()
+        {
+            return View();
+        }
+        public IActionResult shop3()
+        {
+            return View();
+        }
+        public IActionResult error404()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
