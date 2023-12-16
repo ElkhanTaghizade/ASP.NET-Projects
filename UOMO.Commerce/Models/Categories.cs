2c605d4c73656d0cd9ede3ea2f60b4544d39a5e7
ElkhanTaghizade <140736203+ElkhanTaghizade@users.noreply.github.com>
2023-12-16 12:44:16 +0400

    Add files via upload

---
+++ UOMO.Commerce/Models/Categories.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication5.Models
+{
+    public class Categories
+    {
+        [Key] public int Id { get; set; }
+        [Required] [StringLength(50)]public string Name { get; set; }
+        public virtual List<Products> Products { get; set; }
+
+    }
+}
---
+++ UOMO.Commerce/Models/Products.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication5.Models
+{
+    public class Products
+    {
+        [Key] public int Id { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] public float Price { get; set; }
+        [NotMapped] public string Test {  get; set; }
+        public int CategoriesId {  get; set; }
+        [ForeignKey("CategoriesId")]
+        public virtual Categories Categories { get; set; }
+
+
+
+
+
+    }
+}
