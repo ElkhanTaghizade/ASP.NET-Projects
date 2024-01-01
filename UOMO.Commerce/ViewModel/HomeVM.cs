8a437ef40707f8ab67b458cb3d6d25932e52af61
ElkhanTaghizade <140736203+ElkhanTaghizade@users.noreply.github.com>
2024-01-01 16:56:31 +0400

    Add files via upload

---
+++ UOMO.Commerce/ViewModel/HomeVM.cs
@@ -0,0 +1,15 @@
+﻿using WebApplication5.Migrations;
+using WebApplication5.Models;
+using Slider = WebApplication5.Models.Slider;
+
+namespace WebApplication5.ViewModel
+{
+    public class HomeVM
+    {
+        public List<Slider> Slider { get; set; }
+        public List<Products> Products { get; set; }
+
+
+
+    }
+}
